2a2559868d36582426f28ec3b31014be6a59591a
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-03-25 19:43:18 -0400

    mvp and stretch

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import axios from "axios";
+
+
+
+export default function Form(props) {
+
+  return (
+
+    <form onSubmit={props.formSubmit}>
+
+      <label htmlFor="name">
+        <span className="leftSpan">Name</span>
+            <div className="formInput1">
+                <input
+                    className="inputField"
+                    id="name"
+                    type="text"
+                    name="name"
+                    value={props.formState.name}
+                    onChange={props.inputChange}
+                />
+            </div>
+        
+        {props.errors.name.length > 0 ? <p className="error">{props.errors.name}</p> : null}
+      </label>
+
+      <label htmlFor="email">
+        <span className="leftSpan">Email</span>
+            <div className="formInput2">
+                <input
+                    className="inputField"
+                    id="email"
+                    type="text"
+                    name="email"
+                    value={props.formState.email}
+                    onChange={props.inputChange}
+                />
+            </div>
+        {props.errors.email.length > 0 ? (
+          <p className="error"> {props.errors.email}</p>
+        ) : null}
+      </label>
+      
+      <label htmlFor="password" className="password">
+        <span className="leftSpan">Password</span>
+            <div className="formInput3">
+                <input 
+                className="inputField" 
+                id="password" 
+                type="password" 
+                name="password" 
+                onChange={props.inputChange}
+                />
+            </div>
+      </label>
+
+      <label htmlFor="terms" className="terms">
+        <input
+          type="checkbox"
+          name="terms"
+          checked={props.formState.terms}
+          onChange={props.inputChange}
+        />
+        <a href="/" className="rightSpan">Terms and Conditions</a>
+      </label>
+
+      <pre>{JSON.stringify(props.post, null, 2)}</pre>
+
+      <button disabled={props.buttonDisabled}>SUBMIT</button>
+
+    </form>
+  );
+}
